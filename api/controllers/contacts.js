673b3753f80437139fd15c178996ffb334e09794
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-10-18 21:07:32 +0100

    contacts controller sorted

--- api/controllers/contacts.js
+++ api/controllers/contacts.js
@@ -2,23 +2,14 @@ const User = require('../models/user');
 
 
 exports.update = (req, res) => {
-  User.findOneAndUpdate({ _id: req.body.user._id }, (err, user) => {
-    if (err) {
-      res.json('summit went wrong');
-    }
-    user.set({ contacts: req.body.user.contacts });
-    user.save((updateErr, userUpdated) => {
-      if (updateErr) {
-        res.json('could not update');
+  User.updateOne(
+    { _id: req.body.user._id },
+    { contacts: req.body.user.contacts },
+    (err, user) => {
+      if (err) {
+        res.json('summit went wrong');
       }
-      res.json(userUpdated);
-    });
-  })
-    .then(user => {
-      if (!user) {
-        return res.sendStatus(404);
-      }
-      console.log('saved');
       res.json(user);
-    });
+    }
+  );
 };

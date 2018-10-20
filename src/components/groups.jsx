9e032a1191216741fbe313b9a738134bf068b220
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-10-20 14:51:15 +0100

    added groups form, name field, and members select.

--- src/components/groups.jsx
+++ src/components/groups.jsx
@@ -1,15 +1,76 @@
 import React, { Fragment } from 'react';
+import axios from 'axios';
 import NavBar from './navbar';
 
-const Groups = (props) => (
-  <Fragment>
-    <h1>{props.user.firstName}</h1>
-    <h1>{props.user.lastName}</h1>
-    <h1>{props.user.email}</h1>
-    <h1>{props.user.password}</h1>
-    <NavBar />
-    Groups Page
-  </Fragment>
-);
+// import ContactCard from './contactCard';
+
+class Groups extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      contacts: [
+        {
+          name: 'paul',
+          _id: 1,
+        },
+        {
+          name: 'john',
+          _id: 2,
+        },
+      ],
+      groupName: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        Group Page
+        <NavBar />
+        <form>
+
+          Add a group name and choose its members from the drop down
+
+          <div>
+            <input
+              name="groupName"
+              type="text"
+              value={this.state.groupName}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div>
+            <select ref="userInput" defaultValue="">
+              <option value="" disabled>Members</option>
+              {
+                this.state.contacts.map(contact => (
+                  <option
+                    key={contact._id}
+                    value={contact.name}
+                  >
+                    {contact.name}
+                  </option>
+                ))}
+              }
+            </select>
+          </div>
+
+          <div>
+            {this.state.groupName}
+          </div>
+        </form>
+      </Fragment>
+    );
+  }
+}
 
 export default Groups;
